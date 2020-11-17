3a95a6455d896a991e5b5b32c6a81c641409b1e1
Max <maxwil2001@hotmail.com>
2020-11-17 14:46:57 +0100

    plantGrowth mechanic

---
+++ Ith.io Project/Assets/Scripts/Max/Plant.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    public PlantData plantData;
+
+    [Header("how long it takes for the plant to grow")]
+    [SerializeField] private float growthDuration = 0.0f;
+
+    [Header("keeps track if the plant is fully grown or not")]
+    [SerializeField] private bool fullyGrown = false;
+
+    [Header("sprites for every growth stage of the plant from 0 to 1")]
+    public GameObject[] growthSprites = null;
+
+    private float startDuration = 0.0f;
+    private int arrayIndex = 0;
+
+    private void Start()
+    {
+        startDuration = growthDuration;
+    }
+
+    void Update()
+    {
+        TimerDecrease();
+
+        GrowPlant();
+    }
+
+    //decreases the timer for growth of one iteration
+    private void TimerDecrease()
+    {
+        if(growthDuration > 0 && fullyGrown == false)
+        {
+            growthDuration -= plantData.growthSpeed * Time.deltaTime;
+        }
+        else if(growthDuration <= 0)
+        {
+            growthDuration = startDuration;
+        }
+    }
+
+    //turns on the new sprite and turns the older one off
+    private void GrowPlant()
+    {
+        if(growthDuration <= 0 && fullyGrown == false)
+        {
+            growthSprites[arrayIndex].SetActive(false);
+
+            arrayIndex++;
+
+            growthSprites[arrayIndex].SetActive(true);
+        }
+        else if(arrayIndex == growthSprites.Length - 1)
+        {
+            fullyGrown = true;
+        }
+    }
+}
---
+++ Ith.io Project/Assets/Scripts/Max/PlantData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "plantData", menuName = "ScriptableObjects/PlantInformation", order = 1)]
+public class PlantData : ScriptableObject
+{
+    [Header("name of the plant")]
+    public string plantName;
+
+    [Header("the speed at which the plant will grow")]
+    public float growthSpeed;
+
+    [Header("sprites for every growth stage of the plant from 0 to 1")]
+    public GameObject[] growthSprites;
+}
