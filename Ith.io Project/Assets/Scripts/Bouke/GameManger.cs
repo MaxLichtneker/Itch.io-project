84d44b8fbe80fce218a1c695d3ec16cfb17d21a6
Bouke <BoukeWeel@outlook.com>
2020-11-23 12:04:35 +0100

    GameManger script and verkoop

---
+++ Ith.io Project/Assets/Scripts/Bouke/GameManger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour
+{
+    public int money;
+
+    //hoow mutch the selling price is
+    public int sellPriceCarrot;
+    public int sellPriceTomato;
+    public int sellPriceCapace;
+
+    //how mutch I have of this plant
+    [HideInInspector] public int amountCarrot;
+    [HideInInspector] public int amountTomato;
+    [HideInInspector] public int amountCapace;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AddMoney(int amount)
+    {
+        money += amount;
+    }
+}
---
+++ Ith.io Project/Assets/Scripts/Bouke/Verkoop.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Verkoop : MonoBehaviour
+{
+    public int price_Carrot;
+    public int price_Tomato;
+    public int price_Capace;
+
+    private GameManger gm;
+
+    private void Awake()
+    {
+        //get the gamemanger
+        gm = GameManger.FindObjectOfType<GameManger>();
+        if(gm == null)
+        {
+            Debug.LogError("Cant find the gamemanger in this scene!");
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    
+
+    public void VerkoopCarrot()
+    {
+        if(gm.amountCarrot > 0)
+        {
+            gm.amountCarrot -= 1;
+            gm.AddMoney(gm.sellPriceCarrot);
+        }
+    }
+    public void Verkooptomato()
+    {
+        if(gm.amountTomato > 0)
+        {
+            gm.amountTomato -= 1;
+            gm.AddMoney(gm.sellPriceTomato);
+            
+        }
+    }
+    public void Verkoopcapace()
+    {
+
+    }
+
+
+}
