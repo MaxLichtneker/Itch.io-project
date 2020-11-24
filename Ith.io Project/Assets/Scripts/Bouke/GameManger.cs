f1fa1b39b3d81c791733056cd79b05ea17d2e8ea
Bouke <BoukeWeel@outlook.com>
2020-11-24 10:17:57 +0100

    beter max!!!!!!!!!!!!!!!!!

--- Ith.io Project/Assets/Scripts/Bouke/GameManger.cs
+++ Ith.io Project/Assets/Scripts/Bouke/GameManger.cs
@@ -6,46 +6,33 @@ public class GameManger : MonoBehaviour
 {
     public int money;
 
-    //hoow mutch the selling price is
-    public int sellPriceCarrot;
-    public int sellPriceTomato;
-    public int sellPriceCapace;
+    
 
     //how mutch I have of this plant
     public int amountCarrot;
     public int amountTomato;
     public int amountCapace;
-
-    public TMP_Text t_amountCarrot;
-    public TMP_Text t_amountTomato;
-    public TMP_Text t_amountCapace;
-
-    public GameObject sellui;
-    public bool selluiactive;
-
-    public bool ableToWalk;
+    
+    
+    [HideInInspector] public bool sellUiActive;
+    [HideInInspector] public bool ableToWalk;
 
     private void Awake()
     {
-        sellui.SetActive(selluiactive);
+        
     }
 
     private void Update()
     {
-        t_amountCarrot.text = "Carrots In Stock: " + amountCarrot.ToString();
-        t_amountTomato.text = "Tomatos In Stock: " + amountTomato.ToString();
-        t_amountCapace.text = "Capaces In Stock: " + amountCapace.ToString();
-
-        sellui.SetActive(selluiactive);
-
-        ableToWalk = !selluiactive;
+        ableToWalk = !sellUiActive;
     }
+    /// <summary>
+    /// here the money is counted up
+    /// </summary>
+    /// <param name="amount"></param>
     public void AddMoney(int amount)
     {
         money += amount;
     }
-    public void ActivateSellUi()
-    {
-        selluiactive = true;
-    }
+    
 }
--- Ith.io Project/Assets/Scripts/Bouke/Verkoop.cs
+++ Ith.io Project/Assets/Scripts/Bouke/Verkoop.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Verkoop : MonoBehaviour
 {
 
     private GameManger gm;
 
-    
+    //hoow mutch the selling price is
+    public int sellPriceCarrot;
+    public int sellPriceTomato;
+    public int sellPriceCapace;
+
+    public TMP_Text t_amountCarrot;
+    public TMP_Text t_amountTomato;
+    public TMP_Text t_amountCapace;
+
+    public GameObject sellui;
 
     private void Awake()
     {
@@ -17,7 +27,16 @@ public class Verkoop : MonoBehaviour
         {
             Debug.LogError("Cant find the gamemanger in this scene!");
         }
-        
+
+        sellui.SetActive(gm.sellUiActive);
+    }
+    private void Update()
+    {
+        t_amountCarrot.text = "Carrots In Stock: " + gm.amountCarrot.ToString();
+        t_amountTomato.text = "Tomatos In Stock: " + gm.amountTomato.ToString();
+        t_amountCapace.text = "Capaces In Stock: " + gm.amountCapace.ToString();
+
+        sellui.SetActive(gm.sellUiActive);
     }
 
     public void SellCarrot()
@@ -25,7 +44,7 @@ public class Verkoop : MonoBehaviour
         if(gm.amountCarrot > 0)
         {
             gm.amountCarrot -= 1;
-            gm.AddMoney(gm.sellPriceCarrot);
+            gm.AddMoney(sellPriceCarrot);
         }
     }
     public void SellTomato()
@@ -33,7 +52,7 @@ public class Verkoop : MonoBehaviour
         if(gm.amountTomato > 0)
         {
             gm.amountTomato -= 1;
-            gm.AddMoney(gm.sellPriceTomato);
+            gm.AddMoney(sellPriceTomato);
             
         }
     }
@@ -42,12 +61,16 @@ public class Verkoop : MonoBehaviour
         if(gm.amountCapace > 0)
         {
             gm.amountCapace -= 1;
-            gm.AddMoney(gm.sellPriceCapace);
+            gm.AddMoney(sellPriceCapace);
         }
     }
     private void OnMouseDown()
     {
-        gm.ActivateSellUi();
+        ActivateSellUi();
+    }
+    public void ActivateSellUi()
+    {
+        gm.sellUiActive = true;
     }
 
 
