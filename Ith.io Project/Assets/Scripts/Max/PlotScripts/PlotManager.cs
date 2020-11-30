0b444acde27854b01428b79cf609dfb162bea2ae
Max <maxwil2001@hotmail.com>
2020-11-30 11:30:39 +0100

    last push for bouke

--- Ith.io Project/Assets/Scripts/Max/PlotScripts/PlotManager.cs
+++ Ith.io Project/Assets/Scripts/Max/PlotScripts/PlotManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlotManager : MonoBehaviour
 {
+    [Header("all the seeds and the amounts the player has")]
+    public int carrotSeed, cabbageSeed, tomatoSeed;
+
     void Start()
     {
         
