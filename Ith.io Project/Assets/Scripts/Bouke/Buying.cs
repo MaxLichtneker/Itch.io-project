00e544c23af3be17e72c86ad7dded10e814c7bc0
Bouke <BoukeWeel@outlook.com>
2020-11-30 14:54:00 +0100

    Koop circle
    de koop ding is af en de koop circle en de sell bin is een prefab nu

---
+++ Ith.io Project/Assets/Scripts/Bouke/Buying.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Buying : MonoBehaviour
+{
+    private GameManger gm;
+
+    //the cost of buying seeds For the vetebals
+    public int buyingPriceCarrotSeeds;
+    public int buyingPriceTomatoSeeds;
+    public int buyingPriceCapaceSeeds;
+
+    public TMP_Text textCarrotSeedPrice;
+    public TMP_Text textTomatoSeedPrice;
+    public TMP_Text textCapaceSeedPrice;
+
+    public GameObject buyUi;
+
+    private void Awake()
+    {
+        gm = GameManger.FindObjectOfType<GameManger>();
+
+        if(gm == null)
+        {
+            Debug.LogError("Cant Find the GameManger in this scene Make sure the game Manger is in this scene");
+        }
+    }
+    private void Update()
+    {
+        textCarrotSeedPrice.text = "Carrot Seeds Price: " + buyingPriceCarrotSeeds.ToString();
+        textTomatoSeedPrice.text = "Tomato Seeds Price: " + buyingPriceTomatoSeeds.ToString();
+        textCapaceSeedPrice.text = "Capace Seeds Price: " + buyingPriceCapaceSeeds.ToString();
+
+
+        buyUi.SetActive(gm.buyUiActive);
+    }
+
+    public void BuyCarrotSeeds()
+    {
+        if(gm.money >= buyingPriceCarrotSeeds)
+        {
+            gm.money -= buyingPriceCarrotSeeds;
+            PlotManager.plotManagerInstance.carrotSeed++;
+        }
+    }
+    public void BuyTomatoSeeds()
+    {
+        if(gm.money >= buyingPriceTomatoSeeds)
+        {
+            gm.money -= buyingPriceTomatoSeeds;
+            PlotManager.plotManagerInstance.tomatoSeed++;
+        }
+    }
+    /// <summary>
+    /// buy the seeds for money 
+    /// </summary>
+    public void BuyCapaceSeeds()
+    {
+        if (gm.money >= buyingPriceCapaceSeeds)
+        {
+            gm.money -= buyingPriceCapaceSeeds;
+            PlotManager.plotManagerInstance.cabbageSeed++;
+        }
+    }
+
+
+    /// <summary>
+    /// activate the Buy UI
+    /// </summary>
+    public void ActivateBuyUI()
+    {
+        gm.buyUiActive = true;
+    }
+
+    /// <summary>
+    /// exit the Buy UI
+    /// </summary>
+    public void ExitBuyUI()
+    {
+        gm.buyUiActive = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            ActivateBuyUI();
+        }
+    }
+
+
+}
