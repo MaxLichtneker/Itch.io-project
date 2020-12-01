2da479ea7e519d3ba12663ca46317f6cf2b475bd
Max <maxwil2001@hotmail.com>
2020-12-01 10:37:51 +0100

    Pause menu

---
+++ Ith.io Project/Assets/Scripts/Max/Menu/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [Header("panel attached to the canvas")]
+    [SerializeField] private GameObject pausePanel = null;
+
+    [SerializeField] private bool paused = false;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && paused == false)
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0.0f;
+
+            paused = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape) && paused)
+        {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1.0f;
+
+            paused = false;
+        }
+    }
+
+    public void Resume()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1.0f;
+
+        paused = false;
+    }
+
+    public void Controls()
+    {
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
