e8e92a1ad7c2ba682aea58735612ef2a7a97232a
Bouke <BoukeWeel@outlook.com>
2020-12-01 12:46:45 +0100

    MainMenu

---
+++ Ith.io Project/Assets/Scripts/Bouke/MainMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject options;
+    public GameObject controls;
+
+    public void Start()
+    {
+        mainMenu.SetActive(true);
+        options.SetActive(false);
+        controls.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Options()
+    {
+        mainMenu.SetActive(false);
+        options.SetActive(true);
+    }
+    public void Controls()
+    {
+        mainMenu.SetActive(false);
+        controls.SetActive(true);
+    }
+    public void BackToMainMenu()
+    {
+        controls.SetActive(false);
+        options.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
