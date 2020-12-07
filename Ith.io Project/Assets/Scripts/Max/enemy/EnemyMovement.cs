b649457a88734796274cda0d5ec30d04ad7daf51
Max <maxwil2001@hotmail.com>
2020-12-07 13:34:02 +0100

    enemy walks away from player when holding torch

--- Ith.io Project/Assets/Scripts/Max/enemy/EnemyMovement.cs
+++ Ith.io Project/Assets/Scripts/Max/enemy/EnemyMovement.cs
@@ -4,22 +4,53 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    [Header("speed of the enemy")]
     [SerializeField] private float enemySpeed = 0.0f;
+
+    [Header("distance between the enemy and the player")]
+    [SerializeField] private float distance = 0.0f;
+
+    [Header("the transform of the player")]
+    private GameObject playerTransform = null;
+
+    private Torch torch;
 
     private GameObject targetPosition;
 
     private void Start()
     {
+        torch = FindObjectOfType<Torch>();
+
         targetPosition = GameObject.Find("Player");
+
+        playerTransform = GameObject.Find("Player");
     }
 
     void Update()
     {
         Movement();
+
+        if (torch.isEquiped)
+        {
+            AfraidOfTorch();
+        }
     }
 
+    //moves the enemy towards the player
     private void Movement()
     {
         transform.position = Vector2.MoveTowards(transform.position, targetPosition.gameObject.transform.position, enemySpeed * Time.deltaTime);
     }
+
+    //moves the enemy away from the player
+    private void AfraidOfTorch()
+    {
+       distance = Vector3.Distance(gameObject.transform.position, playerTransform.transform.position);
+
+        if(distance > 0.5)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition.gameObject.transform.position, -enemySpeed * 1.5f * Time.deltaTime);
+        }
+    }
+
 }
---
+++ Ith.io Project/Assets/Scripts/Max/enemy/Torch.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : MonoBehaviour
+{
+    [Header("checks if the torch is equipped or not")]
+    public bool isEquiped = false;
+
+    [Header("GameObject with sprite visual")]
+    [SerializeField] private GameObject spriteVisual = null;
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1) && isEquiped == false)
+        {
+            EquipTorch();
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha1) && isEquiped)
+        {
+            UnequipTorch();
+        }
+    }
+
+    //equips the torch
+    private void EquipTorch()
+    {
+        isEquiped = true;
+
+        spriteVisual.SetActive(true);
+    }
+    
+    //unequips the torch
+    private void UnequipTorch()
+    {
+        isEquiped = false;
+
+        spriteVisual.SetActive(false);
+
+    }
+}
