3ef917e1d8d9d4cd64762a27c5ad0f0ed8c97f43
Bouke <BoukeWeel@outlook.com>
2020-12-07 13:19:02 +0100

    Audio Manger af

---
+++ Ith.io Project/Assets/Scripts/Bouke/Audoi/SoundManger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManger : MonoBehaviour
+{
+    public Sounds[] sounds;
+
+    public static SoundManger instance;
+
+    private void Awake()
+    {
+        //make this a instace if there is already a instance destroy the dublecate gameojbect
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
+        //get all the right variable out the sounds script with a foreach
+        foreach (Sounds s in sounds )
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.Clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+    
+    public void Play(string name)
+    {
+        //play sound that was called from a other script
+        Sounds s =  Array.Find(sounds, sound => sound.Name == name);
+        //if there was no sound found with the name given give error message
+        if(s == null)
+        {
+            Debug.LogError("There is no sound with the name " + name);
+            return;
+        }
+        s.source.Play();
+
+    }
+
+}
---
+++ Ith.io Project/Assets/Scripts/Bouke/Audoi/Sounds.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sounds 
+{
+    public string Name;
+    
+
+    public AudioClip Clip;
+
+    [Range(0f, 1f)]
+    public float volume = 1;
+    [Range(.1f, 3f)]
+    public float pitch;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+
+    
+}
