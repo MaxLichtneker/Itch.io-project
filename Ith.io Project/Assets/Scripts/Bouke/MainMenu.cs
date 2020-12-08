3513613a584a0c5a104bb11fdee8ee972e9f37ac
Bouke <BoukeWeel@outlook.com>
2020-12-08 10:20:26 +0100

    nieuwe sound effect

--- Ith.io Project/Assets/Scripts/Bouke/MainMenu.cs
+++ Ith.io Project/Assets/Scripts/Bouke/MainMenu.cs
@@ -19,20 +19,24 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
+        SoundManger.instance.Play("Click");
         SceneManager.LoadScene(1);
     }
     public void Options()
     {
+        SoundManger.instance.Play("Click");
         mainMenu.SetActive(false);
         options.SetActive(true);
     }
     public void Controls()
     {
+        SoundManger.instance.Play("Click");
         mainMenu.SetActive(false);
         controls.SetActive(true);
     }
     public void BackToMainMenu()
     {
+        SoundManger.instance.Play("Click");
         controls.SetActive(false);
         options.SetActive(false);
         mainMenu.SetActive(true);
@@ -40,5 +44,9 @@ public class MainMenu : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+    private void OnApplicationQuit()
+    {
+        SoundManger.instance.Play("Click");
     }
 }
