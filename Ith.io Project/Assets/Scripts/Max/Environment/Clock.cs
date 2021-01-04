8fb0e1df79c6c392ed536029ed52320006558d8f
Max <maxwil2001@hotmail.com>
2021-01-04 14:52:35 +0100

    Clock

---
+++ Ith.io Project/Assets/Scripts/Max/Environment/Clock.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour
+{
+    private DayNightCyle dayNightCyle;
+
+    [Header("Image that shows what the time currently is")]
+    [SerializeField] private Image currentTimeVisual;
+
+
+    [Header("Tmpro text that shows what day it is")]
+    [SerializeField] private TextMeshProUGUI dayText = null;
+
+    void Start()
+    {
+        dayNightCyle = FindObjectOfType<DayNightCyle>();
+    }
+
+    void Update()
+    {
+        SetDayToText();
+
+        SetImageFill();
+    }
+
+    private void SetDayToText()
+    {
+        dayText.text = dayNightCyle.days.ToString();
+    }
+
+    private void SetImageFill()
+    {
+        currentTimeVisual.fillAmount = dayNightCyle.time / 300.0f;
+    }
+
+}
