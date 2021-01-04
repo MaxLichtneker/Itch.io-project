b63b35a38ecd0e8f33ce0df774612d47beb9ba96
Max <maxwil2001@hotmail.com>
2021-01-04 11:54:20 +0100

    Added layer sorting and font

---
+++ Ith.io Project/Assets/Scripts/Max/Player/LayerSorter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerSorter : MonoBehaviour
+{
+    [SerializeField] private int sortingOrder = 5000;
+
+    [SerializeField]
+    private int offset = 0;
+
+    private Renderer playerRenderer;
+
+    void Start()
+    {
+        playerRenderer = GetComponent<Renderer>();
+    }
+
+    private void LateUpdate()
+    {
+        Sorter();
+    }
+
+    //sorts the layer of the player depending on the position
+    private void Sorter()
+    {
+        playerRenderer.sortingOrder = (int)(sortingOrder - transform.position.y - offset);
+    }
+}
