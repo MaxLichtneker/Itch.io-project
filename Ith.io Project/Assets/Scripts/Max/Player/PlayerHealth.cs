732faffe1e01d6c4ad1326e85fa419f48c28acb7
Max <maxwil2001@hotmail.com>
2021-01-05 13:43:39 +0100

    health

--- Ith.io Project/Assets/Scripts/Max/Player/PlayerHealth.cs
+++ Ith.io Project/Assets/Scripts/Max/Player/PlayerHealth.cs
@@ -6,26 +6,30 @@ public class PlayerHealth : MonoBehaviour
 {
     [Range(0,3)]
     [Header("amount of health the player has")]
-    [SerializeField] private float health = 0.0f;
+    [SerializeField] private int health = 0;
 
     [Header("images of the health")]
     [SerializeField] private GameObject[] healthImages;
 
     void Start()
     {
-        
+        health = healthImages.Length;
     }
 
     void Update()
     {
-        
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.collider.CompareTag("Enemy"))
+        if (collision.CompareTag("Enemy"))
         {
-            health--;
+            if (health > 0)
+            {
+                health--;
+                healthImages[health].SetActive(false);
+            }
         }
     }
+
 }
