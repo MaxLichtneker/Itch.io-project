9bdace040830cbb712c0866c00b6de9bd39c6137
Max <maxwil2001@hotmail.com>
2021-01-05 13:23:21 +0100

    start with health stuff

---
+++ Ith.io Project/Assets/Scripts/Max/Player/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [Range(0,3)]
+    [Header("amount of health the player has")]
+    [SerializeField] private float health = 0.0f;
+
+    [Header("images of the health")]
+    [SerializeField] private GameObject[] healthImages;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Enemy"))
+        {
+            health--;
+        }
+    }
+}
