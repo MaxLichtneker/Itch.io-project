a0dca4c751bad7e0445af132f77c9e98b9f97fb0
Bouke <BoukeWeel@outlook.com>
2021-01-12 13:23:21 +0100

    lights go around

--- Ith.io Project/Assets/Scripts/Bouke/DayNightCyle.cs
+++ Ith.io Project/Assets/Scripts/Bouke/DayNightCyle.cs
@@ -10,6 +10,12 @@ public class DayNightCyle : MonoBehaviour
 
     public int days;
 
+    public List<Transform> lightPos;
+   
+
+    public float t;
+    
+
     //public int Days => days;
 
     [Header("set here the Time in what is a full day and night cyle in seconds")]
@@ -19,27 +25,41 @@ public class DayNightCyle : MonoBehaviour
 
     private bool canChangeDay = true;
 
-    
+    private void Start()
+    {
+
+        //transform.position = lightPos[1].position;
+        //isAtPos2 = true;
+        t = 0.50f;
+    }
 
     private void Update()
     {
-        if(time > maxTime)
+        if (time > maxTime)
         {
             time = 0;
         }
         //change the day number when it is mid night
-        if((int)time == maxTime / 2 && canChangeDay)
+        if ((int)time == maxTime / 2 && canChangeDay)
         {
             canChangeDay = false;
             days++;
-            
+
         }
         // make it able to change the day agian
-        if((int)time == maxTime / 2 + 5)
+        if ((int)time == maxTime / 2 + 5)
         {
             canChangeDay = true;
         }
 
+        if (t >= 1)
+        {
+            t = 0;
+        }
+    
+        t += Time.deltaTime / 300;
+            transform.position = Vector3.Lerp(lightPos[0].position, lightPos[1].position,t);
+        
         
 
         time += Time.deltaTime;
